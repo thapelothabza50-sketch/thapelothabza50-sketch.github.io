44892714ad0c9ba333cf57c333eedd9fafd2e961
Tapelo Magwaza <thapelothabza50@gmail.com>
2025-12-23 04:48:05 +0200

    Confirmed link fix for index.html

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api/shop', shopRoutes);
 app.use('/api/orders', orderRoutes);      
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const startServer = async () => {
